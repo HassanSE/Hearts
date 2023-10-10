33ab0585fb148c91899d8b1c0c630c816e075bb7
Muhammad Hasssan <hassankhll@hotmail.com>
2023-10-10 19:05:13 +0500

    Refactor symbol property code on Rank to use rawValue for numeric ranks (2-10)

--- Hearts.playground/Contents.swift
+++ Hearts.playground/Contents.swift
@@ -19,25 +19,8 @@ extension Rank: CustomStringConvertible {
             return "Q"
         case .jack:
             return "J"
-        case .ten:
-            return "10"
-        case .nine:
-            return "9"
-        case .eight:
-            return "8"
-        case .seven:
-            return "7"
-        case .six:
-            return "6"
-        case .five:
-            return "5"
-        case .four:
-            return "4"
-        case .three:
-            return "3"
-        case .two:
-            return "2"
-            
+        default:
+            return String(rawValue)
         }
     }
     
