464b91b835653bba5e940435f253110e4f4b01c9
Muhammad Hasssan <hassankhll@hotmail.com>
2023-10-12 23:38:05 +0500

    Add framework project for Hearts as playground is no longer a suitable project type

---
+++ Hearts/Hearts/Hearts.h
@@ -0,0 +1,18 @@
+//
+//  Hearts.h
+//  Hearts
+//
+//  Created by Muhammad Hassan on 12/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hearts.
+FOUNDATION_EXPORT double HeartsVersionNumber;
+
+//! Project version string for Hearts.
+FOUNDATION_EXPORT const unsigned char HeartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hearts/PublicHeader.h>
+
+
