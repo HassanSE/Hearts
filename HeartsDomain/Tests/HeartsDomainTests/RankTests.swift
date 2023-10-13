a6166cdb9cb1e10b20c381914e11efaa8685a0b5
Muhammad Hasssan <hassankhll@hotmail.com>
2023-10-14 00:25:41 +0500

    Add test for the order of card ranks

--- HeartsDomain/Tests/HeartsDomainTests/RankTests.swift
+++ HeartsDomain/Tests/HeartsDomainTests/RankTests.swift
@@ -9,8 +9,16 @@ import XCTest
 @testable import HeartsDomain
 
 final class RankTests: XCTestCase {
+    
     func test_there_are_13_ranks() {
         let ranks = Card.Rank.allCases
         XCTAssertEqual(ranks.count, 13)
+    }
+    
+    func test_ranks_order() {
+        let ranks = Card.Rank.allCases.sorted(by: >)
+        XCTAssertEqual(ranks.first, Card.Rank.ace)
+        XCTAssertEqual(ranks.last, Card.Rank.two)
+        XCTAssertEqual(ranks, [.ace, .king, .queen, .jack, .ten, .nine, .eight, .seven, .six, .five, .four, .three, .two])
     }
 }
